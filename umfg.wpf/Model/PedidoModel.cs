061512b96b056eef6a2a2cc2a8cf78b2679647ba
Gustavo <gustavoalbuquerque3050@gmail.com>
2026-02-11 20:47:37 -0300

    V0.3

---
+++ umfg.wpf/Model/PedidoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umfg.wpf.Model
+{
+    public sealed class PedidoModel
+    {
+
+        private Guid _id = Guid.NewGuid();
+
+        private decimal _total = 0.0m;
+
+        private ObservableCollection<ProdutoModel> _produtos = [];
+
+        public Guid id { get => _id; set => _id = value; }
+
+        public decimal total { get => _total; set => _total = value; }
+
+
+        public ObservableCollection<ProdutoModel> Produtos
+        { get => _produtos; set => _produtos = value; }
+
+
+
+    }
+}
---
+++ umfg.wpf/Model/ProdutoModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace umfg.wpf.Model
+{
+    public class ProdutoModel
+    {
+        private Guid _id = Guid.NewGuid();
+        private ImageSource _imagem;
+        private string _referencia;
+        private string _descricao;
+        private decimal _valor;
+
+
+
+        public Guid id { get => _id; set => _id = value; }
+
+        public ImageSource imagem { get => _imagem; set => _imagem = value; }
+
+        public string referencia { get => _referencia; set => _referencia = value; }
+
+        public string descricao { get => _descricao; set => _descricao = value; }
+
+        public decimal valor { get => _valor; set => _valor = value; }
+
+        
+
+    }
+}
